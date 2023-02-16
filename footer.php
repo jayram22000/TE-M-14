1561c68742e72dc9ae885b27108b9aae3ba7dd72
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-16 10:01:43 +0200

    universal header

--- footer.php
+++ footer.php
@@ -8,7 +8,9 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 //* Footer Styles */
-
+.body{
+  background:black;
+}
 .footer {
   background-color:black;
   padding:20%;
@@ -18,14 +20,16 @@
 
 .footer h4 {
   color: #333;
-  font-size: 18px;
+  font-size: 15px;
   font-weight: bold;
   margin-bottom: 20px;
 }
 
 .footer p {
-  color: #666;
+  color:silver;
   margin-bottom: 20px;
+  margin:0;
+  font-size:15px;
 }
 
 .social-icons li {
@@ -42,7 +46,7 @@
   font-size: 24px;
   display: inline-block;
   line-height: 1;
-  border-radius: 50%;
+  border-radius: 30%;
   width: 40px;
   height: 40px;
   text-align: center;
@@ -56,7 +60,7 @@
 
 .map-responsive {
   overflow: hidden;
-  padding-bottom: 40%;
+  padding-bottom: 56.25%;
   position: relative;
   height: 0;
 }
@@ -65,58 +69,118 @@
   left: 0;
   top: 0;
   height: 50%;
-  width: 50%;
+  width: 100%;
   position: absolute;
-  border: 0;
-}
 
+}
+.container{
+  max-width:100%;
+  border:10% solid;
+  margin:0 auto;
+  height:2%;
+  padding: 0 15;
+  float:left;
+
+}
+.col-md-4{
+  position: relative;
+  width:100%;
+  height:1%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+  box-sizing: border-box;
+  float:left;
+}
+.col-md-6{
+  position: relative;
+  width:100%;
+  height:1%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+  box-sizing: border-box;
+}
+.col-md-5{
+  position: relative;
+  width:100%;
+  height:1%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+  box-sizing: border-box;
+}
 /* Logo styles */
 
 .footer img {
   max-width: 200px;
   margin-bottom: 20px;
 }
+@media (min-width: 80%) 
+  .col-md-4 {
+    float: right;
+    width: 33.3333%;
+  }
+  .col-md-6 {
+    float: right;
+    width: 66.6667%;
+  }
+  .col-md-5 {
+    float: center;
+    width: 66.6667%;
+  }
+  .col-md-4 {
+    float: left;
+    width: 66.6667%;
+  }
 
-@media (max-width: 50%) {
+
+@media (max-width: 30%) {
   .footer {
     text-align: center;
   }
 
   .footer h4 {
-    margin-top: 30px;
+    margin-top: 20px;
   }
 
   .map-responsive {
-    margin-bottom: 10px;
+  margin-bottom: 10px;
+  overflow: hidden;
+  padding-bottom: 40%; /* adjust this value to your desired size */
+  position: relative;
+  height: 0;
   }
 }
 
 </style>
 </head>
+<body>
 <footer class="footer">
   <div class="container">
-    <div class="row">
       <div class="col-md-4">
-        <h4>Connect with Us</h4>
-        <ul class="social-icons">
+      <h4>Follow us</h4>
+      <ul class="social-icons">
           <li><a href="https://www.facebook.com/your-restaurant" target="_blank"><i class="fa fa-facebook"></i></a></li>
           <li><a href="https://www.instagram.com/your-restaurant" target="_blank"><i class="fa fa-instagram"></i></a></li>
           <li><a href="https://twitter.com/your-restaurant" target="_blank"><i class="fa fa-twitter"></i></a></li>
-        </ul>
-        <img src="path/to/your/logo.png" alt="Your Restaurant Logo">
+          <li><a href="https://www.youtube.com"/your-restaurant" target="_blank"><i class="fa fa-youtube"></i></a></li>
+          <img src="path/to/your/logo.png" alt="Your Restaurant Logo">
+      </ul>
       </div>
-      
+       <div clas="col-md-6">
         <h4>Find Us</h4>
         <p>13100,Hämeenlinna FINLAND</p>
-        <div class="col-md-4">
-        <div class="map-responsive">            c 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30677.13959557388!2d23.995459099999998!3d61.278440700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468ee59e7dd29a31%3A0xf511c3e7a9213906!2sS-Market%20Superi!5e0!3m2!1ssv!2sfi!4v1676457121365!5m2!1ssv!2sfi" width="680" height="880" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="map-responsive"> 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30677.13959557388!2d23.995459099999998!3d61.278440700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468ee59e7dd29a31%3A0xf511c3e7a9213906!2sS-Market%20Superi!5e0!3m2!1ssv!2sfi!4v1676457121365!5m2!1ssv!2sfi" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
         <h4>Contact Us</h4>
-        <p>Email:salsarestaurant@gamil.com</p>
-        <p>Phone: 123-456-7890</p>
+        <p>Email:salsarestaurant@gmail.com</p>
+        <p>Phone:123-456-7890</p>
+        <p><?php echo date('F j, Y, g:i a');?></p>
+
       </div>
-    </div>
-  </div>
-</footer>
+  </footer>
+        </body>
+        </html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title><?php echo $title ?></title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#"><img src="path/to/restaurant-logo.png" alt="Salsa Restaurants"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Online Order</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Opening Hours</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
