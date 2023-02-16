9b0234155762255ccd29be4453e8786755f739ca
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-16 16:54:29 +0200

    universal header almost done

--- header.php
+++ header.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <body>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title><?php echo $title ?></title>
+    <title><?php echo $title="header.php" ?></title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" type="text/css" href="styling.css">
+    </head>
     <!-- Bootstrap CSS -->
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#"><img src="path/to/restaurant-logo.png" alt="Salsa Restaurants"></a>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">   
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+    <a class="navbar-brand" href="#"><img src="logo.png" alt="Salsa Restaurants"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="#">Home</a>
+          <a class="nav-link" href="home.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Menu</a>
@@ -37,8 +39,13 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Catering</a>
           </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#">The Event Loft</a>
+          </li>
+        
           <!-- Language selection drop-down -->
           <li class="nav-item">
+          <a class="nav-link">
                         <select id="language-selector" onchange="changeLanguage()">
                             <option value="en" selected>English</option>
                             <option value="sv">Svenska</option>
@@ -66,3 +73,6 @@
     </div>
   </div>
 </nav>
+  
+   
+  
---
+++ home.php
@@ -0,0 +1,13 @@
+<?php
+$title= "Home";
+ include "header.php"?><!-- universal header !-->
+ <body>
+
+</div>
+
+</div>
+
+</div> 
+
+
+<?php include "footer.php" ?>   <!-- universal footer !-->
