8ba289abf106822ca0e90cc3b3a1077580d95ff0
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-17 08:43:21 +0200

    delete the logo from amrit and update on footer

--- footer.php
+++ footer.php
@@ -63,6 +63,8 @@
   padding-bottom: 56.25%;
   position: relative;
   height: 0;
+  padding:auto;
+  margin:auto;
 }
 
 .map-responsive iframe {
@@ -71,6 +73,9 @@
   height: 50%;
   width: 100%;
   position: absolute;
+  margin:auto;
+  padding:auto;
+  text allign:center;
 
 }
 .container{
@@ -122,7 +127,7 @@
     width: 33.3333%;
   }
   .col-md-6 {
-    float: right;
+    float: left;
     width: 66.6667%;
   }
   .col-md-5 {
@@ -130,8 +135,8 @@
     width: 66.6667%;
   }
   .col-md-4 {
-    float: left;
-    width: 66.6667%;
+    float:right;
+    width:auto
   }
 
 @media (max-width: 30%) {
@@ -152,11 +157,16 @@
   height: 0;
   }
 }
-
+.navbar-brand img {
+    width: 60px;
+    height: 60px;
+    margin: auto;
+    background-color: transparent;
+   
+  }
 </style>
-</head>
-<body>
-<footer class="footer">
+<footer>
+
   <div class="container">
       <div class="col-md-4">
       <h4>Follow us</h4>
@@ -165,24 +175,23 @@
           <li><a href="https://www.instagram.com/your-restaurant" target="_blank"><i class="fa fa-instagram"></i></a></li>
           <li><a href="https://twitter.com/your-restaurant" target="_blank"><i class="fa fa-twitter"></i></a></li>
           <li><a href="https://www.youtube.com" target="_blank"><i class="fa fa-youtube"></i></a></li>
-          
-          <img src="jjawhxxp_0.png" alt="logo">
-          
+    
       </ul>
       </div>
+      <div class="col-md-5">
+        <h4>Contact Us</h4>
+       <p> <a class="navbar-brand" href=""><img src="logo.png" alt="Salsa Restaurants"></P>
+        <p>Email:salsarestaurant@gmail.com</p>
+        <p>Phone:123-456-7890</p>
+        <p><?php echo date('F j, Y, g:i a');?></p>
        <div clas="col-md-6">
         <h4>Find Us</h4>
         <p>13100,Hämeenlinna FINLAND</p>
         <div class="map-responsive"> 
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30677.13959557388!2d23.995459099999998!3d61.278440700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468ee59e7dd29a31%3A0xf511c3e7a9213906!2sS-Market%20Superi!5e0!3m2!1ssv!2sfi!4v1676457121365!5m2!1ssv!2sfi" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-        <div class="col-md-5">
-        <h4>Contact Us</h4>
-        <p>Email:salsarestaurant@gmail.com</p>
-        <p>Phone:123-456-7890</p>
-        <p><?php echo date('F j, Y, g:i a');?></p>
-
+       </div>
+      
       </div>
-  </footer>
-        </body>
-        </html>
+      </div>
+
+    </footer>
