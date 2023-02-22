168e0044e206b0568a4782245f83d4161ed6aaef
amrit-kandel-web <98876854+amrit-kandel-web@users.noreply.github.com>
2023-02-22 17:02:50 +0200

    Update menu.php
    done menu page

--- menu.php
+++ menu.php
@@ -62,6 +62,11 @@
 		text-align: center;
 		color: white;
 	}
+	button{
+		height: 5%;
+		width:20%
+	}
+	
 </style>
 
 </head>
@@ -71,44 +76,62 @@
 		<h1>Salsa Restaurant Menu</h1>
 		<div class="menu">
 			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1570197571499-166b36435e9f?ixlib=rb-4.>0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2FsYWRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
-					alt="Salmon Avocado Salad">
-				<p>£4,20</p>
-				<h3>Salmon Avocado Salad</h3>
+				<img src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid" width=100%, height=100%;
+					alt="spicy beef burger">
+					<button type="button" class="btn btn-primary">order</button>
+					<h3>Burger</h3><p><br>£4,20</p>
+				
+				<br>
+				<br>
 
 			</div>
 			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1611171711925-92171c63fc4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNhbGFkc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+				<img src="https://images.unsplash.com/photo-1611171711925-92171c63fc4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNhbGFkc3xlbnwwfHwwfHw%3D&auto" width=100%, height=100%;
 					alt="Mixed Green Salad with apple and Goat Cheese">
-				<p>£3,99</p>
-				<h3>Mixed Green Salad with apple and Goat Cheese</h3>
-
+					<button type="button" class="btn btn-primary">order</button>
+					<h3>apple Salad</h3><p><br>£3,99</p>
+				
+				
+				<br>
+				<br>
 			</div>
 			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1568158879083-c42860933ed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNhbGFkc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
+				<img src="https://images.unsplash.com/photo-1568158879083-c42860933ed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNhbGFkc3xlbnwwfHwwfHw%3D&auto"width=100%, height=100%;
 					alt="Mixed Green Salad with apple and Goat Cheese">
-				<p>£5,99</p>
-				<h3>Mixed Green Salad with apple and Goat Cheese</h3>
-			</div>
-			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1655536259633-05170abf4b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYyfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
-					alt="Lemon Balm Popsicles">
-				<p>£4,99</p>
-				<h3>Lemon Balm Popsicles</h3>
-
-			</div>
-			<div class="menu-item">
-				<img src="https://plus.unsplash.com/premium_photo-1671980819443-e8bbeec85840?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMwfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
-					alt="Lemon Balm Popsicles">
-				<p>£7,99</p>
-				<h3>Lemon Balm Popsicles</h3>
+					<button type="button" class="btn btn-primary">order</button>
+					<h3>Mixed Green Salad</h3><p><br>£5,99</p>
+				
+				<br>
 				
 			</div>
 			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1671637292704-bbc6fe8310a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ4fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+				<img src="https://images.unsplash.com/photo-1655536259633-05170abf4b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYyfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto"width=100%, height=100%;
 					alt="Lemon Balm Popsicles">
-				<p>£7,99</p>
-				<h3>Lemon Balm Popsicles</h3>
+					<button type="button" class="btn btn-primary">order</button>
+					<h3>Lemon Balm Popsicles</h3> <p><br>£4,99</p>
+				
+				<br>
+				<br>
+
+			</div>
+			<div class="menu-item">
+				<img src="https://plus.unsplash.com/premium_photo-1671980819443-e8bbeec85840?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMwfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto" width=100%, height=100%;
+					alt="Lemon Balm Popsicles">
+					<button type="button" class="btn btn-primary">order</button>
+					<h3>Lemon Balm Popsicles</h3> <p><br>£7,99</p>
+				
+				
+				<br>
+				<br>
+			</div>
+			<div class="menu-item">
+				<img src="https://images.unsplash.com/photo-1671637292704-bbc6fe8310a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ4fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto"width=100%, height=100%;
+					alt="Lemon Balm Popsicles">
+					<button type="button" class="btn btn-primary">order</button> 
+					<h3>Lemon Balm Popsicles</h3> <p><br>£7,99</p>
+				
+				
+				
 				
 			</div>
 		</div>
@@ -116,4 +139,32 @@
 </body>
 
 </html>
+<br>
+<br>
+<br>
 <?php include "footer1.php"	?>
+
+    <div id="ocFooterWrapper">
+ <div id="supWrapperToPreventFeedbackFlickering" data-enable-star-rating="False">
+  <section id="supFeedbackWrapper" class="prrFeedbackWrapper">
+ <div id="supColumnWrapper" class="prrFeedbackWrapper">
+   <div id="supFeedBackCountainer">
+   <section id="supFB" class="ocFB">
+   <div id="supFBCont">
+   <form action="javascript:void(0);" id="extendedFeedbackForm" name="smartFeedbackForm"
+    class="smartFeedbackForm supStickyFeedbackMarginBottom">
+    <div class="ocSmartFeedbackBegin extendedFeedbackArticleInfoUseful" role="region"
+  aria-label="provide feedback about this article">
+    <h2 id="beginFeedbackHeader" class="boldFont extendedFeedbackHeader"> was the
+    service good?</h2>
+   <div class="feedbackButtons" role="group" aria-labelledby="beginFeedbackHeader">
+    <button
+    class="ocFeedbackButton supStickyFeedbackBotton feedbackSelection feedbackButtonBlue"
+    name="buttonYes" type="button" data-bi-id="buttonYes"
+ data-bi-mto>Yes</button>
+ <button
+    class="ocFeedbackButton supStickyFeedbackBotton feedbackSelection feedbackButtonBlue"
+  name="buttonNo" type="button" data-bi-id="buttonNo"
+  data-bi-mto>No</button>
+   </div>
+   </div>
