5edde07cd0d219020666df83c320943223880bc0
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-22 07:48:10 +0200

    created the crude folder

---
+++ crude/db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "password";
+$dbname = "";
+// creating connection 
+$conn = new mysqli ($servername, $username, $password, $dbname);
+// check connection 
+if ($conn->connect_error){
+    die("Connection failed; " . $conn->connect_error);
+}
+?>
