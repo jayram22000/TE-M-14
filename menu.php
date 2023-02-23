e1193b818765e4b72b4ceb88834381616a06b4b6
amrit-kandel-web <98876854+amrit-kandel-web@users.noreply.github.com>
2023-02-23 13:58:27 +0200

    done
    change

--- menu.php
+++ menu.php
@@ -2,168 +2,146 @@
 <title><?php echo $title="menu" ?></title>
 
 <style>
-	.menu.container {
-		width: 100%;
-	}
 
+<<<<<<< Updated upstream
 	body {
 		background-image: url("image/food.jpg");
 		background-repeat: no-repeat;
 		background-size: cover;
 		color: white;
 	}
+=======
+    body{
+      background-color:none;
+      font-family: latha;
+      color:whitesmoke;
+      background:url(food.jpg)no-repeat;
+      background-size: cover;
+  }
+/* Center the header */
+h1 {
+  text-align: center;
+}
+>>>>>>> Stashed changes
 
+/* Add spacing between items */
+.card {
+  margin-bottom: 20px;
+}
 
-	h1 {
-		text-align: center;
-	}
+/* Style the order button */
+.btn-primary {
+  background-color: #f57f17;
+  border-color: #f57f17;
+}
 
-	.menu {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
+/* Add some spacing at the top */
+.container {
+  margin-top: 50px;
+}
 
-	}
+/* Style the images to fit properly */
+.card-img-top {
+  width: 100%;
+  height: 225px;
+  object-fit: cover;
+}
 
-	.menu>div {
-		margin: 20px;
-		padding: 20px;
-		font-size: 30px;
-		text-align: center;
-		
-	}
+/* Style the text inside the cards */
+.card-text, .card-title {
+  font-size: 18px;
+}
 
-	.menu-item {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-		padding: 50px, 50px;
-		margin: 100px;
-		width: 35%;
-		margin-bottom: 50px;
-	}
+/* Style the cards on small devices */
+@media (max-width: 575px) {
+  .card-img-top {
+    height: 150px;
+  }
+  .card-text, .card-title {
+    font-size: 16px;
+  }
+}
 
-	.menu-item img {
-		display: flex;
-		margin: auto;
-		align-items: center;
+/* Style the cards on medium devices */
+@media (min-width: 576px) and (max-width: 767px) {
+  .card-img-top {
+    height: 175px;
+  }
+}
 
-	}
+/* Style the cards on large devices */
+@media (min-width: 992px) {
+  .card-img-top {
+    height: 250px;
+  }
+}
 
-	.menu-item>h3 {
-		
-		text-align: center;
-		color: white;
-	}
+    </style>
 
-	.menu-item>p {
-		margin-top: auto;
-		text-align: center;
-		color: white;
-	}
-	button{
-		height: 5%;
-		width:20%
-	}
-	
-</style>
-
-</head>
-
-<body>
-	<div class="menu.container">
-		<h1>Salsa Restaurant Menu</h1>
-		<div class="menu">
-			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid" width=100%, height=100%;
-					alt="spicy beef burger">
-					<button type="button" class="btn btn-primary">order</button>
-					<h3>Burger</h3><p><br>£4,20</p>
-				
-				<br>
-				<br>
-
-			</div>
-			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1611171711925-92171c63fc4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNhbGFkc3xlbnwwfHwwfHw%3D&auto" width=100%, height=100%;
-					alt="Mixed Green Salad with apple and Goat Cheese">
-					<button type="button" class="btn btn-primary">order</button>
-					<h3>apple Salad</h3><p><br>£3,99</p>
-				
-				
-				<br>
-				<br>
-			</div>
-			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1568158879083-c42860933ed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHNhbGFkc3xlbnwwfHwwfHw%3D&auto"width=100%, height=100%;
-					alt="Mixed Green Salad with apple and Goat Cheese">
-					<button type="button" class="btn btn-primary">order</button>
-					<h3>Mixed Green Salad</h3><p><br>£5,99</p>
-				
-				<br>
-				
-			</div>
-			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1655536259633-05170abf4b8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYyfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto"width=100%, height=100%;
-					alt="Lemon Balm Popsicles">
-					<button type="button" class="btn btn-primary">order</button>
-					<h3>Lemon Balm Popsicles</h3> <p><br>£4,99</p>
-				
-				<br>
-				<br>
-
-			</div>
-			<div class="menu-item">
-				<img src="https://plus.unsplash.com/premium_photo-1671980819443-e8bbeec85840?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMwfHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto" width=100%, height=100%;
-					alt="Lemon Balm Popsicles">
-					<button type="button" class="btn btn-primary">order</button>
-					<h3>Lemon Balm Popsicles</h3> <p><br>£7,99</p>
-				
-				
-				<br>
-				<br>
-			</div>
-			<div class="menu-item">
-				<img src="https://images.unsplash.com/photo-1671637292704-bbc6fe8310a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDQ4fHhqUFI0aGxrQkdBfHxlbnwwfHx8fA%3D%3D&auto"width=100%, height=100%;
-					alt="Lemon Balm Popsicles">
-					<button type="button" class="btn btn-primary">order</button> 
-					<h3>Lemon Balm Popsicles</h3> <p><br>£7,99</p>
-				
-				
-				
-				
-			</div>
-		</div>
-	</div>
-</body>
-
-</html>
-<br>
-<br>
-<br>
-<?php include "footer1.php"	?>
-
-    <div id="ocFooterWrapper">
- <div id="supWrapperToPreventFeedbackFlickering" data-enable-star-rating="False">
-  <section id="supFeedbackWrapper" class="prrFeedbackWrapper">
- <div id="supColumnWrapper" class="prrFeedbackWrapper">
-   <div id="supFeedBackCountainer">
-   <section id="supFB" class="ocFB">
-   <div id="supFBCont">
-   <form action="javascript:void(0);" id="extendedFeedbackForm" name="smartFeedbackForm"
-    class="smartFeedbackForm supStickyFeedbackMarginBottom">
-    <div class="ocSmartFeedbackBegin extendedFeedbackArticleInfoUseful" role="region"
-  aria-label="provide feedback about this article">
-    <h2 id="beginFeedbackHeader" class="boldFont extendedFeedbackHeader"> was the
-    service good?</h2>
-   <div class="feedbackButtons" role="group" aria-labelledby="beginFeedbackHeader">
-    <button
-    class="ocFeedbackButton supStickyFeedbackBotton feedbackSelection feedbackButtonBlue"
-    name="buttonYes" type="button" data-bi-id="buttonYes"
- data-bi-mto>Yes</button>
- <button
-    class="ocFeedbackButton supStickyFeedbackBotton feedbackSelection feedbackButtonBlue"
-  name="buttonNo" type="button" data-bi-id="buttonNo"
-  data-bi-mto>No</button>
-   </div>
-   </div>
+  <body>
+    <div class="container my-5">
+      <h1 class="text-center mb-5"> Salsa Restaurant Menu</h1>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+          <div class="card">
+            <img src="image/food1.jpg" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title">€3.99</h5>
+              <p class="card-text">Burger.</p>
+              <a href="" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="image/food3.jpg" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title">€4.99</h5>
+              <p class="card-text">Green salad</p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <img src="image/food2.jpg" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title">€2.99</h5>
+              <p class="card-text">Juice</p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <div class="card">
+            <img src="image/food3.jpg" class="card-img-top" alt="Image 4">
+            <div class="card-body">
+              <h5 class="card-title">Item 4</h5>
+              <p class="card-text"></p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="image/food4.jpg" class="card-img-top" alt="food6.jpg">
+            <div class="card-body">
+              <h5 class="card-title">5</h5>
+              <p class="card-text"></p>
+              <a href="" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img src="image/food5.jpg" class="card-img-top" alt="Image 5">
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <p class="card-text">Description of item 6.</p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+            </div>
+          </div>
+        </div>
+        <?php include "footer1.php" ?>
