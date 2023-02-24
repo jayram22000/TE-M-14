ee13a417a4f8d651ac6c88b89459f0101af1cce9
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-24 13:06:25 +0200

    finnlly connection with database server

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+// Connect to the database
+$servername = "db";
+$username = "data2";
+$password = "zack";
+$dbname = "data2";
+//creating connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check for errors
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+?>
+
+
