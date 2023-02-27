99ba203aebb75ca9b85a6f5995f692a5c791dd36
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-27 12:25:44 +0200

    created the retrieve.php

---
+++ retrieve.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Restaurant Data</title>
+	
+</head>
+<body>
+	<h1>Restaurant Data</h1>
+
+	<h2>Menu</h2>
+	<table>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Description</th>
+			<th>Price</th>
+			<th>Image URL</th>
+		</tr>
+		<?php
+		
