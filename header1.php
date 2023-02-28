9c480f60f9472e38528ce378ed7aea3e03d1c476
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-28 14:03:34 +0200

    create a index.php file to link whole crude function on a website if we need

--- header1.php
+++ header1.php
@@ -98,26 +98,4 @@
         </div>
     </nav>
     
-    <!-- just a normal language switcher function  -->
-    <script>
-  function changeLanguage() {
-  var selectedLanguage = document.getElementById("language-selector").value;
-  switch (selectedLanguage) {
-    case "en":
-      window.location.href = "https://www.hamk.fi/?lang=en";
-      document.getElementById("container").style.backgroundColor = "white"; // change background color to white
-      break;
-    case "sv":
-      window.location.href = "https://www.hamk.fi/?lang=sv";
-      document.getElementById("container").style.backgroundColor = "blue"; // change background color to blue
-      break;
-    case "fi":
-      window.location.href = "https://www.hamk.fi/?lang=fi";
-      document.getElementById("container").style.backgroundColor = "green"; // change background color to green
-      break;
-    default:
-      break;
-  }
-}
-
-    </script>
+   
