e9e02158dc8bcf278306a38a6a0898c09a64e9fa
bev @nun <86957585+aabbeegg@users.noreply.github.com>
2023-02-28 15:34:07 +0200

    added the db.php connection

--- crude/retrieve.php
+++ crude/retrieve.php
@@ -5,9 +5,7 @@
 	
 </head>
 <body>
-	<h1>Restaurant Data</h1>
 
-	<h2>Menu</h2>
 	<table>
 		<tr>
 			<th>ID</th>
@@ -18,12 +16,17 @@
 		</tr>
 		<?php
         // Connect to the database
+		/*
 		$servername = "db";
 		$username = "data2";
 		$password = "zack";
 		$dbname = "data2";
 		$conn = new mysqli($servername, $username, $password, $dbname);
-       
+       */
+	  $title = "reading data from database";
+	  include "../header1.php";
+	  include 'db.php';
+	  $sql = "select * from menu";
 		// Check connection
 		if (!$conn) {
 			die("Connection failed: " . mysqli_connect_error());
