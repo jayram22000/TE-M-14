f57122e4896e2e74572e7f28cd6f86537b897483
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-03-01 15:10:04 +0200

    link and make a new folder of js.site

--- header1.php
+++ header1.php
@@ -6,14 +6,11 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title><?php echo $title="header" ?></title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
-    </head>
-  
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
-    <script src="../TE@M-14/crude/site.js"> </script>
+    <script src="../Te@m-14/js/site.js"> </script> 
+  
   </head>
   <style>
   
@@ -67,6 +64,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+           
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto mb-auto mb-lg-auto">
                     <li class="nav-item">
@@ -87,15 +85,18 @@
                    
                     <!-- Language selection drop-down -->
                     <li class="nav-item">
-                        <select id="language-selector" onchange="changeLanguage()" class="nav-link">
+                    <select id="language-selector" onchange="changeLanguage()" class="nav-link">
+
                             <option value="en" selected>English</option>
                             <option value="sv">Svenska</option>
                             <option value="fi">Suomi</option>
                         </select>
+                       
                     </li>
                 </ul>
             </div>
+            </nav>
         </div>
-    </nav>
+  
     
    
