499a539d126528efaf4c9d4e928ff8672c1da975
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-03-02 21:27:33 +0200

    add css style for a table and include header1.php perfectyly working

--- crude/menuedit.php
+++ crude/menuedit.php
@@ -1,4 +1,70 @@
-<?php include "db.php";
+<?php include "../header1.php" ?>   <!-- universal footer !-->
+<?php include "db.php";?> 
+ <style>
+      body{
+      background-color:none;
+      font-family: latha;
+      color:white;
+      background:url(../image/food.jpg)no-repeat;
+      background-size: cover;
+  }
+<style>
+table {
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+th, td {
+  text-align: left;
+  padding: 8px;
+  
+}
+
+th {
+  background-color: #f2f2f2;
+  color:black;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background-color: #ddd;
+}
+
+input[type="text"], input[type="number"] {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  width: 100%;
+}
+
+input[type="submit"] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="submit"]:hover {
+  background-color: #3e8e41;
+}
+
+@media screen and (max-width: 600px) {
+  input[type="submit"] {
+    width: 100%;
+    margin-bottom: 5px;
+  }
+}
+</style>
+
+<?php
 
 // Retrieve data from the database
   $sql = "SELECT * FROM menu";
@@ -6,7 +72,7 @@
 
   if (mysqli_num_rows($result) > 0) {
     echo "<table>";
-    echo "<tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Image URL</th></tr>";
+    echo "<tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Image URL</th><th>Action</th></tr>";
     while($row = mysqli_fetch_assoc($result)) {
         echo "<form method='post'>";
         echo "<tr>";
@@ -27,7 +93,7 @@
     } else {
       echo "0 results";
     }
-  
-    // Close database connection
+  // Close database connection
     mysqli_close($conn);
   ?>
+  <?php include "../footer2.php" ?>
