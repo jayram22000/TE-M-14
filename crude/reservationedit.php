9176e1bf958f7c8b6c4e88098a1c6651a6914ec8
mwendwa22000 <115934391+mwendwa22000@users.noreply.github.com>
2023-03-02 15:53:24 +0200

    changes

---
+++ crude/reservationedit.php
@@ -0,0 +1,34 @@
+<style>
+	table {
+		width: 100%;
+		margin-bottom: 1rem;
+		color: #212529;
+	}
+	th,
+	td {
+		padding: 0.75rem;
+		vertical-align: top;
+		border-top: 1px solid #dee2e6;
+	}
+	th {
+		background-color: #f8f9fa;
+		font-weight: 700;
+	}
+	tr:nth-child(even) {
+		background-color: #f2f2f2;
+	}
+	form {
+		display: inline;
+	}
+	input[type="submit"] {
+		background-color: transparent;
+		color: #007bff;
+		border: none;
+		cursor: pointer;
+	}
+	input[type="submit"]:hover {
+		text-decoration: underline;
+	}
+</style>
+
+
