5785a8d978edffdd6bbbe506cc0b0d0686a653da
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-03-02 21:38:45 +0200

    done all correction has been done

--- crude/menuedit.php
+++ crude/menuedit.php
@@ -65,35 +65,65 @@ input[type="submit"]:hover {
 </style>
 
 <?php
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+  // Retrieve the form data
+  $id = $_POST['id'];
+  $name = $_POST['name'];
+  $description = $_POST['description'];
+  $price = $_POST['price'];
+  $image_url = $_POST['image_url'];
+
+  // Update the menu item in the database
+  $sql = "UPDATE menu SET name='$name', description='$description', price='$price', image_url='$image_url' WHERE id='$id'";
+  if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully.";
+  } else {
+    echo "Error updating record: " . mysqli_error($conn);
+  }
+}
+
+if (isset($_POST['delete'])) {
+  // Retrieve the form data
+  $id = $_POST['id'];
+
+  // Delete the menu item from the database
+  $sql = "DELETE FROM menu WHERE id='$id'";
+  if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully.";
+  } else {
+    echo "Error deleting record: " . mysqli_error($conn);
+  }
+}
 
 // Retrieve data from the database
-  $sql = "SELECT * FROM menu";
-  $result = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM menu";
+$result = mysqli_query($conn, $sql);{
 
-  if (mysqli_num_rows($result) > 0) {
-    echo "<table>";
-    echo "<tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Image URL</th><th>Action</th></tr>";
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<form method='post'>";
-        echo "<tr>";
-        echo "<td>" . $row["id"] . "</td>";
-        echo "<td><input type='text' name='name' value='" . $row["name"] . "'></td>";
-        echo "<td><input type='text' name='description' value='" . $row["description"] . "'></td>";
-        echo "<td><input type='number' name='price' value='" . $row["price"] . "'></td>";
-        echo "<td><input type='text' name='image_url' value='" . $row["image_url"] . "'></td>";
-        echo "<td>";
-        echo "<input type='hidden' name='id' value='" . $row["id"] . "'>";
-        echo "<input type='submit' name='submit' value='Update'>";
-        echo "<input type='submit' name='delete' value='Delete'>";
-        echo "</td>";
-        echo "</tr>";
-        echo "</form>";
-      }
-      echo "</table>";
-    } else {
-      echo "0 results";
-    }
-  // Close database connection
-    mysqli_close($conn);
-  ?>
-  <?php include "../footer2.php" ?>
+}if (mysqli_num_rows($result) > 0) {
+  echo "<table>";
+  echo "<tr><th>ID</th><th>Name</th><th>Description</th><th>Price</th><th>Image URL</th><th>Action</th></tr>";
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "<form method='post'>";
+    echo "<tr>";
+    echo "<td>" . $row["id"] . "</td>";
+    echo "<td><input type='text' name='name' value='" . $row["name"] . "'></td>";
+    echo "<td><input type='text' name='description' value='" . $row["description"] . "'></td>";
+    echo "<td><input type='number' name='price' value='" . $row["price"] . "'></td>";
+    echo "<td><input type='text' name='image_url' value='" . $row["image_url"] . "'></td>";
+    echo "<td>";
+    echo "<input type='hidden' name='id' value='" . $row["id"] . "'>";
+    echo "<input type='submit' name='submit' value='Update'>";
+    echo "<input type='submit' name='delete' value='Delete'>";
+    echo "</td>";
+    echo "</tr>";
+    echo "</form>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+
+// Close database connection
+mysqli_close($conn);
+?>
